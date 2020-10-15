6d6eadd3790e4bfb960cdf2efb1f62907f42f589
franri <franrizzo.98@gmail.com>
2020-10-15 17:50:30 -0300

    agrego endpoints para webhooks

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    console.log(req);
+    res.send('Hello World!');
 });
+
+app.get('/webhook', (req, res) => {
+    console.log(req);
+    res.send('Hello World!');
+  });
+
+  app.post('/webhook', (req, res) => {
+      console.log(req);
+      res.send(JSON.stringify({
+          "status": "success",
+          "chatbot_response": "todo ok"
+      }));
+  });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
