3995844d9a9ca2de7df1064401795283a0ca4c48
franri <franrizzo.98@gmail.com>
2020-10-19 16:31:53 -0300

    imprimo todos los campos

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 
 const express = require('express');
 const app = express();
+const util = require('util')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-    console.log(req);
-    res.send(JSON.stringify(req));
+    console.log(req.query);
+    console.log(req.body);
+    const d = {"get": req.query, "body": req.body};
+    res.send(util.inspect(d, false, null, true /* enable colors */));
 });
 
 app.get('/webhook', (req, res) => {
-    console.log(req);
-    res.send(JSON.stringify(req));
+    console.log(req.query);
+    console.log(req.body);
+    const d = {"get": req.query, "body": req.body};
+    res.send(util.inspect(d, false, null, true /* enable colors */));
   });
 
   app.post('/webhook', (req, res) => {
-      console.log(req);
-      res.send(JSON.stringify(req));
+      console.log(`req.ip: ${req.ip}`);
+      console.log(`req.ips: ${req.ips}`);
+      console.log(`req.query: ${req.query}`);
+      console.log(`req.params: ${req.params}`);
+      console.log(`req.body: ${req.body}`);
+      const data = {"get": req.query, "body": req.body};
+      const string = util.inspect(d, false, null, true /* enable colors */)
+      console.log(s);
+      res.send(s);
   });
 
 const port = process.env.PORT || 3000;
